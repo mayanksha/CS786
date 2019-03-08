afbcb2847f31ffd0eae8a88b52021d77ecec84b3
Mayank Sharma <mayank8019@gmail.com>
2019-03-08 15:28:00 +0530

    Finished q2.py with argparser

--- A2/q2.py
+++ A2/q2.py
@@ -12,22 +12,26 @@ size = 20
 import argparse 
 parser = argparse.ArgumentParser()
 parser.add_argument("-n" ,"--num_objects", help="The number of objects to generate", default="10")
-parser.add_argument("--conjunction",required='--feature' not in sys.argv, help="0 for little endian (default), 1 for big endian", default="0")
-parser.add_argument("--feature",required='--conjunction' not in sys.argv, help="0 for color different, 1 for shape different", default="0")
+parser.add_argument("--conjunction", help="Supply this flag to simulate conjunction search", action="store_true")
+parser.add_argument("--feature", help="0 for color different, 1 for shape different", )
 args = parser.parse_args()
 
 def generateImage(x, y, n_channels):
     img_height, img_width = x, y 
     image = np.zeros((img_height, img_width, n_channels), dtype=np.uint8)
+    trans_mask = image[:,:,3] == 0
+    image[trans_mask] = [255, 255, 255, 255]
     return image
 
-pt1 = (np.random.randint(size / 2, size), np.random.randint(0, size / 2))
-pt2 = (np.random.randint(0, size / 2), np.random.randint(size, size * 1.5))
-pt3 = (np.random.randint(size, size * 1.5), np.random.randint(size, size * 1.5))
+# pt1 = (np.random.randint(size / 2, size), np.random.randint(0, size / 2))
+# pt2 = (np.random.randint(0, size / 2), np.random.randint(size, size * 1.5))
+# pt3 = (np.random.randint(size, size * 1.5), np.random.randint(size, size * 1.5))
 def generateTriangle(color):
+    pt1 = (int(0.75 * size), int(0.5 * size))
+    pt2 = (int(0.5 * size), int(1.5 * size))
+    pt3 = (int(1.5 * size), int(1.5 * size))
     image = generateImage(size * 1.5, size * 1.5, 4) 
 
-    print(pt1, pt2, pt3)
     triangle_cnt = np.array( [pt1, pt2, pt3] )
     cv2.fillConvexPoly(image, triangle_cnt, color)
     return image
@@ -49,17 +53,21 @@ def placeImage(l_img, s_img, offsets):
 
     alpha_s = 1 + s_img[:, :, 3] / 255.0
     alpha_l = 1.0 - alpha_s
-    # print("x = %d, y = %d" %(x1, y1))
     for c in range(0, 3):
         l_img[y1:y2, x1:x2, c] = (alpha_s * s_img[:, :, c] + alpha_l * l_img[y1:y2, x1:x2, c])
     return l_img
 
 if __name__ == '__main__':
-    try:
-        num_objects = int(sys.argv[1]) 
-    except:
-        num_objects = 20 
-  
+    print("")
+    if '--conjunction' in sys.argv and '--feature' in sys.argv:
+        print("Both feature and conjunction not allowed at same time!")
+        sys.exit(-1)
+    if '--conjunction' not in sys.argv and '--feature' not in sys.argv:
+        print("Atleast one of --conjunction or --feature flag is needed")
+        print("Do python q2.py --help for help.")
+        sys.exit(-1)
+ 
+    num_objects = int(args.num_objects)
     grid_size = 1 + num_objects / 2 
     pixels = (size * 3) * grid_size
 
@@ -88,19 +96,44 @@ if __name__ == '__main__':
                 break
   
     # print(chosen_locations)
-    print("Image shape = (%d, %d)"%(image.shape[0], image.shape[1])) 
-    for i in range(0, num_objects - 1):
-        triangle = generateTriangle(green)
-        tri_cent = (chosen_locations[i][0], chosen_locations[i][1])
-        # print(_x[i], _y[i])
-        image = placeImage(image, triangle, tri_cent)
 
-    square = generateSquare(red)
-    sqr_cent = (chosen_locations[-1][0], chosen_locations[-1][1])
-    image = placeImage(image, square, sqr_cent)
-
-    cv2.imshow("image.png", image)
-    cv2.waitKey()
+    if args.feature != None: 
+        print("-------- FEATURE SEARCH --------")
+        for i in range(0, num_objects - 1):
+            triangle = generateTriangle(blue)
+            tri_cent = (chosen_locations[i][0], chosen_locations[i][1])
+            image = placeImage(image, triangle, tri_cent)
+        if args.feature == "0":
+            triangle = generateTriangle(red)
+            tri_cent = (chosen_locations[-1][0], chosen_locations[-1][1])
+            image = placeImage(image, triangle, tri_cent)
+        else:
+            square = generateSquare(blue)
+            sqr_cent = (chosen_locations[-1][0], chosen_locations[-1][1])
+            image = placeImage(image, square, sqr_cent)
+    elif args.conjunction != None: 
+        print("-------- CONJUNCTION SEARCH --------")
+        # So that triangles and squares occur in nearly equal number
+        num_triangles = np.random.randint(0.25 * num_objects, 0.75 * num_objects) 
+        num_squares = num_objects - num_triangles - 1
+        print("Number of Triangles (Randomly chosen) = ", num_triangles)
+        print("Number of Squares (Randomly chosen) = ", num_squares + 1)
+        for i in range(0, num_triangles):
+            triangle = generateTriangle(blue)
+            tri_cent = (chosen_locations[i][0], chosen_locations[i][1])
+            image = placeImage(image, triangle, tri_cent)
+        for i in range(0, num_squares):
+            square = generateSquare(red)
+            sqr_cent = (chosen_locations[i + num_triangles][0], chosen_locations[i + num_triangles][1])
+            image = placeImage(image, square, sqr_cent)
+        square = generateSquare(blue)
+        sqr_cent = (chosen_locations[-1][0], chosen_locations[-1][1])
+        image = placeImage(image, square, sqr_cent)
 
     img = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+
+    cv2.imshow("image.png", image)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+
     cv2.imwrite("./image.png", img)
